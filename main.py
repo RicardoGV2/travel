7c11be85bb682e3b428112380e7159d5f5ea7e7e
protemo <jose.gomez.chivas@hotmail.com>
2024-06-01 02:11:09 -0600

    fixing error

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def create_network_with_top_votes(data, top_voted):
     previous_node = None
 
     for date in data:
-        for time in data[date]]:
+        for time in data[date]:
             top_option = top_voted[date][time]
             time_node = f"{date}_{time}_{top_option}"
             G.add_node(time_node, label=f"{time}\n{top_option}", shape="box")
