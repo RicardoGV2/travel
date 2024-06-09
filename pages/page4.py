ed1f69055edadf6a3516b8668654687184d46c7f
protemo <jose.gomez.chivas@hotmail.com>
2024-06-09 12:56:14 -0600

    updating page4

--- pages/page4.py
+++ pages/page4.py
@@ -1,6 +1,15 @@
+from navigation import make_sidebar
 import streamlit as st
 import json
 import os
+from streamlit_autorefresh import st_autorefresh
+
+make_sidebar()
+
+# Redirigir a la página de inicio de sesión si no está logueado
+if not st.session_state.get("logged_in", False):
+    st.warning("Please log in to access this page.")
+    st.stop()
 
 # Paths to the data and votes files
 data_file = "data.json"
@@ -18,6 +27,15 @@ def load_data(file_path):
 
 # Page layout
 st.title("JSON Data Viewer")
+
+# Add a setting to pause or continue autorefresh
+st.sidebar.title("Settings")
+auto_refresh = st.sidebar.checkbox("Enable Auto Refresh", value=True)
+refresh_interval = st.sidebar.number_input("Refresh Interval (seconds)", min_value=1, max_value=60, value=7) if auto_refresh else None
+
+# Autorefresh every 'refresh_interval' seconds if enabled
+if auto_refresh and refresh_interval:
+    st_autorefresh(interval=refresh_interval * 1000, key="datarefresh")
 
 # Section to view data.json
 st.header("Data JSON")
