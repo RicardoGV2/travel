f66d08decac03d3c7b8d6e63f0fe172ec4a9b008
RicardoGV2 <114529344+RicardoGV2@users.noreply.github.com>
2024-06-11 00:01:47 +0000

    fixing url

--- pages/page4.py
+++ pages/page4.py
@@ -10,6 +10,8 @@ import streamlit.components.v1 as components
 make_sidebar()
 
 components.iframe("https://lottie.host/4f1d6ae5-0dfa-41b3-878c-134c1ab5ef63/boemJK6OPy.json")
+components.iframe("https://lottie.host/4f1d6ae5-0dfa-41b3-878c-134c1ab5ef63/boemJK6OPy.json")
+
 
 
 
