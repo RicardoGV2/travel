306ce0ae378b5b618fe4678a9db00277370a5d7a
RicardoGV2 <114529344+RicardoGV2@users.noreply.github.com>
2024-06-11 23:24:27 +0000

    a

--- pages/page4.py
+++ pages/page4.py
@@ -121,8 +121,8 @@ st.markdown("""
         float: right;
         vertical-align: top;
         height: auto;
-        padding-top: 10px !important;
-        padding-bottom: 10px !important;
+        padding-top: -10px !important;
+        padding-bottom: 20px !important;
     }
     .item-container {
         display: flex;
